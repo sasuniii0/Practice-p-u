7dd64ce2adb7237f594f00566e970dad1a6e9eeb
sasuniii0 <sasuniwijerathne@gmail.com>
2025-10-16 11:37:51 +0530

    done fundamentals

---
+++ practice.py
@@ -0,0 +1,56 @@
+#Q1
+print("Hello, World! Welcome to Python.")
+
+#Q2
+print("""
+Twinkle, twinkle, little star,
+How I wonder what you are!
+""")
+
+#Q3
+name = "Sasuni"
+age = 21
+height = 170
+is_student = True
+
+print("Name:", name , "Age:", int(age), "Height:", int(height), "cm", "Student:", is_student)
+
+#Q4
+num = "45"
+print(int(num) + 10)
+
+#Q5
+user_input = input("What is your favorite food? ")
+print("Wow! I also like " + user_input)
+
+#Q6
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+difference = num1 - num2
+print("The difference is:", difference)
+
+product = num1 * num2
+print("The product is:", product)
+
+quotient = num1 / num2
+print("The quotient is:", quotient)
+
+#Q7
+print(
+    """
+Harry said, "Python is awesome!"\nThis is on a new line.
+This is a tab ->\t <- here
+"""
+)
+
+#Q8
+input = input("Enter a number: ")
+print("square of the number is:", int(input)**2)
+print("cube of the number is:", int(input)**3)
+
+#Q9
+#done
